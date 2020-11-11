9e0c9b1f241c1129b18de9458297c06e91889400
vityavovk5 <vityavovk55@gmail.com>
2020-11-11 20:34:36 +0200

    end

---
+++ js/main.js
@@ -0,0 +1,101 @@
+/*console.log("helli");
+
+//alert("sdfsdf");
+ var a = prompt ("a =", 42);
+ alert (`a = ${a}`);
+ console.log(`a = ${a}`);
+
+function fn()
+{
+    console.log("helli");
+}
+function fa()
+{
+    alert("sdfsdf");
+}
+ 
+//number
+var n = 42;
+var f = 34.22;
+console.log(typeof(n));
+console.log(typeof n);
+
+ // string
+ var s = "test";
+ console.log(typeof(s));
+ console.log(s.length);
+ console.log(s[2]);
+ var q = 'my name is "vitya"';
+ console.log(q);
+ var r = `my 'name' ${n} is "vitya"`;
+ console.log(r);
+ var t = "fifi" + "ssdfsdf";
+ console.log(t);
+ var u = "fifi" + q;
+ console.log(u);
+
+ //boolean
+ var b = true;
+ console.log(b);
+ console.log(typeof(b));
+
+ //null
+ var l = null;
+ console.log(l);
+ console.log(l == null);
+
+ //NaN не число 
+ var y = NaN;
+ console.log(isNaN(y));
+ console.log(typeof(y));
+
+ //Infinity 
+ var k = Infinity;
+ console.log(k);
+
+ //undefined
+ console.log(typeof(c));
+ var c = 'sdf';
+ console.log(typeof(c));
+
+ if (typeof c != 'undefined' && c != c) {
+    console.log(c);
+ }
+ 
+var v = 5;
+if (v == 5) {
+    var s = 1;
+    console.log(v);
+    {
+    console.log(`s =`,s);
+}
+
+} else {
+    console.log("vsdf");
+}
+
+var x = 5;
+if (x < 5) {
+    console.log(`x < 5`);
+} else if (x < 10) {
+    console.log(`x < 10`);
+}
+else {
+    console.log(`x >= 10`);
+}
+*/
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Маловато' );
+    break;
+  case 4:
+    alert( 'В точку!' );
+    break;
+  case 5:
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( "Нет таких значений" );
+}
